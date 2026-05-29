556789e3adce5a3d0eb7e15e0b9bbad875247b73
Shahrukh-codes <mahommadshahrukh7786@gmail.com>
2026-05-29 21:41:47 +0530

    Refactor Terraform configuration for AWS r

--- Terraform-first-task/main.tf
+++ Terraform-first-task/main.tf
@@ -69,18 +69,7 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-# EC2 2
-resource "aws_instance" "my_instance2" {
-  ami = "ami-02d26659fd82cf299"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  key_name = "terr-vpc-key"
 
-  tags = {
-    Name = "my_instance2"
-  }
-}
 
 # EC2
 resource "aws_instance" "my_instance" {
